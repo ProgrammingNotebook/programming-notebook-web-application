0cd6bab0ffc5d7c1ac0aa1d63968f7acd7047458
Gagandeep Singh <gagan.sngh14@gmail.com>
2024-08-06 08:18:27 +0530

    Added header

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
 import Head from 'next/head';
 import UnderConstruction from '@/app/ui/under-construction';
+import Header from '@/app/components/header';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Programming Notebook',
+}
 
 export default function Home() {
-  return (
-    <>
-        <Head>
-            <title>Website Under Construction</title>
-            <meta name="description" content="Welcome to the homepage" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-            <UnderConstruction />
-          </div>
-        </main>
-    </>
-  );
+    return (
+        <>
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <Header/>
+                <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                    <UnderConstruction/>
+                </div>
+            </main>
+        </>
+    );
 }
